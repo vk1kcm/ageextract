e503e5b923cad65779787c4700d5006d9a835271
Carl Makin <carl@stagecraft.cx>
2025-03-20 18:12:39 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ageextract.py
+
+A python script to breakout FIE XML into age categories and results.
+
+Since we're not a large state, our age tournaments are quite small, and it's pretty much impossible to get enough fencers in each age group to run an event.  So, we pool them together into one big event, which is typically run as two rounds of poules, and then separate them out into age rankings from the final results.
+
+Our competition rules state, that eligibility for a age group is based on your age at the beginning of the year.  Also, we don't use exact birthdays anymore, for some small privacy protection, and instead use a birthdate of 1/1/\<year>.  Both of these assumptions are hardcoded in.
+
+Carl,
+ACT Fencing Association,
+2025.
---
+++ ageextract.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+#
+# ageextract.py
+# Process FIE XML files for a mixed age event and break out the results into age groups.
+#
+# Assumptions:
+# . Results will be 1,2,3,4,5... ie no dual bronze or third place.
+#
+# Carl Makin
+#
+# Age Ranges
+# U9 - 7,8 
+# U11 - 9,10
+# U13 - 11,12
+# U15 - 13,14
+# U17 - 15,16
+# U20 - 17,18, 19
+# U23 - 20, 21, 22
+# 40+
+
+import argparse
+# import re
+import xml.etree.ElementTree as ET
+import sqlite3
+from sqlite3 import Error
+
+numfencers = 0
+numimported = 0
+numfailed = 0
+failedlist = []
+
+parser = argparse.ArgumentParser()
+parser.add_argument("xmlfile", help="The xml input file in FIE 2019+ format")
+# parser.add_argument("attendees", help="File to store attending members")
+args = parser.parse_args()
+
+eventxml = ET.parse(args.xmlfile)
+root = eventxml.getroot()
+# print("Root: ",root.tag, root.attrib['Date'])
+
+eventname = root.attrib['TitreLong']
+eventweapon = root.attrib['Arme']
+if eventweapon == 'F':
+    eventweapon = "Foil"
+elif eventweapon == 'E':
+    eventweapon = "Epee"
+elif eventweapon == 'S':
+    eventweapon = "Sabre"
+else:
+    eventweapon = "Unknown"
+print("Event:", eventname)
+print("Weapon:", eventweapon)
+
+(evday, evmon, evyear) = root.attrib['Date'].split(".")
+print("Event date:", evday, "/", evmon, "/", evyear)
+
+dbconn = sqlite3.connect(':memory:')
+dbcurs = dbconn.cursor()
+dbcurs.execute('''CREATE TABLE fencers (name TEXT, age INTEGER, gender TEXT, classification INTEGER);''')
+
+fencerlist = root.find("Tireurs")
+for fencer in fencerlist.iter("Tireur"):
+#    print(fencer.tag, fencer.attrib)
+    numfencers = numfencers + 1
+    nom = fencer.get('Nom')
+    prenom = fencer.get('Prenom')
+    name = prenom + " " + nom
+    birthday = fencer.get('DateNaissance')
+    (bday, bmon, byear) = birthday.split(".")
+    age = int(evyear) - int(byear) - 1
+    gender = fencer.get('Sexe')
+    classification = fencer.get('Classement')
+#    print(prenom, " ", nom, " - ", gender, " : ", age, " = ", classification, " :::: ", evyear, " ", byear)
+    try:
+        x = classification.isnumeric()
+        dbcurs.execute('''
+            INSERT INTO fencers VALUES (?, ?, ?, ?);''', (name, age, gender, classification))
+        dbconn.commit()
+        numimported = numimported + 1
+    except (AttributeError):
+        numfailed = numfailed + 1
+        status = fencer.get('Statut')
+        failedlist.append([name, age, gender, classification, status])
+        next
+
+print("Number of fencers: ", numfencers)
+print("Number imported: ", numimported)
+print("Number failed: ", numfailed)
+print()
+print("Failed entries")
+print(",\"Name\",\"Age\",\"Gender\",\"Event Ranking\",\"Status\"")
+for failed in failedlist:
+    print(",", str(failed).strip("[]").replace("'", "\"").replace(", ", ","), sep="")
+print()
+
+print("All Fencers in classification order")
+print("\"Event Ranking\",\"Name\",\"Age\",\"Gender\",\"Event Ranking\"")
+dbcurs.execute('SELECT * from fencers ORDER BY classification')
+fencerlist = dbcurs.fetchall()
+for thisfencer in fencerlist:
+    print(thisfencer[3], ",", str(thisfencer).strip("()").replace("'", "\"").replace(", ", ","), sep="")
+print()
+
+def getresults(minage, maxage, category):
+    print(category, "Women - Ages", minage, "to", maxage)
+    dbcurs.execute("SELECT * from fencers WHERE age BETWEEN ? AND ? AND gender='F' ORDER BY classification",(minage, maxage))
+    fencerlist = dbcurs.fetchall()
+    ageclass = 0
+    print("\"Age Ranking\",\"Name\",\"Age\",\"Gender\",\"Event Ranking\"")
+    for thisfencer in fencerlist:
+        ageclass = ageclass + 1
+        print(ageclass, ",", str(thisfencer).strip("()").replace("'", "\"").replace(", ", ","), sep="")
+    print()
+
+    print(category, "Men - Ages", minage, "to", maxage)
+    dbcurs.execute("SELECT * from fencers WHERE age BETWEEN ? AND ? AND gender='M' ORDER BY classification",(minage, maxage))
+    fencerlist = dbcurs.fetchall()
+    ageclass = 0
+    print("\"Age Ranking\",\"Name\",\"Age\",\"Gender\",\"Event Ranking\"")
+    for thisfencer in fencerlist:
+        ageclass = ageclass + 1
+        print(ageclass, ",", str(thisfencer).strip("()").replace("'", "\"").replace(", ", ","), sep="")
+    print()
+
+getresults(1, 8, "U9")
+getresults(1, 10, "U11")
+getresults(9, 12, "U13")
+getresults(11, 14, "U15")
+getresults(13, 16, "U17")
+getresults(15, 19, "U20")
+getresults(17, 22, "U23")
+getresults(39, 99, "Veteran")
+
